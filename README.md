229de1b574162d8c9edd7aa4603ab3200fd68889
Herve07h22 <contact@camilab.co>
2023-11-03 15:19:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Smallchat
+
+TLDR: writing a very simple IRC server is an experience everybody should do
+
+# Installation
+
+```
+yarn
+yarn start
+```
+
+# How to use it ?
+
+Open a new terminal, and launch a netcat client with :
+```
+nc 127.0.0.1 8124
+``` 
+
+Run 2 or 3 clients to chat together.
+
+![Smallchat](smallchat.png "A conversation").
+
---
+++ src/smallchat.ts
@@ -0,0 +1,104 @@
+import net from "node:net";
+
+// client : nc 127.0.0.1 8124
+
+const WELCOME_MESSAGE =
+  "Welcome to smallchat 🔆 \r\nStart with /nick <your nickname> to join the chat.\r\n";
+
+const NICKNAME_BEFORE_CHAT =
+  "Before sending any message, start with /nick <your nickname> to join the chat.\r\n";
+
+const SMALLCHAT_PORT = 8124;
+
+class SmallChatClient {
+  public state: "connected" | "disconnected" = "connected";
+  public name?: string;
+
+  constructor(private socket: net.Socket, private server: SmallChatServer) {
+    this.logger("client connected");
+    this.state = "connected";
+
+    socket.on("end", () => {
+      this.logger("client disconnected");
+      this.state = "disconnected";
+      this.name &&
+        this.server.sendMessageToOthers(
+          `${this.name} has left the chat 😿\r\n`,
+          this.name
+        );
+    });
+
+    socket.on("data", (data) =>
+      this.processIncomingMessage(data.toLocaleString())
+    );
+
+    this.sendMessage(WELCOME_MESSAGE);
+  }
+
+  processIncomingMessage = (message: string) => {
+    if (message.startsWith("/nick ")) {
+      const name = message.slice(6).trim();
+      if (this.server.nameAlreadyUsed(name)) {
+        this.sendMessage(`${name} is already used ❌ \r\n`);
+        return;
+      }
+      this.name = name;
+      this.server.sendMessageToOthers(
+        `${this.name} has joined the chat 🚀\r\n`,
+        this.name
+      );
+      this.sendMessage(`Hello ${this.name} ! \r\n`);
+      this.sendMessage(
+        `${this.server.nbOfConnectedUsers()} user(s) connected. \r\n`
+      );
+      return;
+    }
+    if (!this.name) {
+      this.sendMessage(NICKNAME_BEFORE_CHAT);
+      return;
+    }
+    this.server.sendMessageToOthers(`${this.name} > ${message}`, this.name);
+    this.logger(message);
+  };
+
+  sendMessage = (message: string) => {
+    this.state === "connected" && this.socket.write(message);
+  };
+
+  logger = (log: string) => {
+    console.log(`${this.name || "unknown"} | ${log}`);
+  };
+}
+
+class SmallChatServer {
+  private server: net.Server;
+  private clients: SmallChatClient[] = [];
+  constructor() {
+    this.server = net.createServer((c) => {
+      this.clients.push(new SmallChatClient(c, this));
+    });
+
+    this.server.on("error", (err) => {
+      throw err;
+    });
+
+    this.server.listen(SMALLCHAT_PORT, () => {
+      console.log("server started");
+    });
+  }
+
+  sendMessageToOthers = (message: string, from: string) => {
+    this.clients
+      .filter((client) => client.name && client.name !== from)
+      .forEach((client) => client.sendMessage(message));
+  };
+
+  nameAlreadyUsed(name: string) {
+    return this.clients.some((client) => client.name === name);
+  }
+
+  nbOfConnectedUsers = () =>
+    this.clients.filter((client) => client.state === "connected").length;
+}
+
+const server = new SmallChatServer();
